4ef13f2146afe720e1ca3f1835898fb3ba365121
SHyguymoll <shymollet@gmail.com>
2025-04-23 21:07:12 -0700

    thresholding a color in the frame

--- camera_mouse.py
+++ camera_mouse.py
@@ -6,6 +6,8 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 cap : cv.VideoCapture = None
+lower_red = np.array([160,20,70])
+upper_red = np.array([190,255,255])
 
 def start_camera():
     global cap
@@ -25,7 +27,20 @@ def get_virtual_position():
     if not ret:
         print("Frame failed to recieve.")
         return
+    hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV_FULL)
+    
+    isolated_frame = cv.inRange(hsv_frame, lower_red, upper_red)
+    cv.imshow("not red", frame)
+    cv.imshow("red", isolated_frame)
+
 
 def end_camera():
     global cap
     cap.release()
+
+start_camera()
+while True:
+    get_virtual_position()
+    if ord("q") == cv.waitKey(1):
+        end_camera()
+        break
