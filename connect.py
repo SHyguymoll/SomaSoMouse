97948f79b8d482079d25c0a9f180da56ad1bc9e0
SHyguymoll <shymollet@gmail.com>
2025-05-11 13:51:39 -0700

    connect script now works as a loop with state explicitly activated and all that, more portable and perfect for a ui

--- connect.py
+++ connect.py
@@ -1,35 +1,50 @@
 import asyncio
+from enum import Enum
 from bleak import BleakClient, BleakGATTCharacteristic
 
 address = "f8:2e:0c:a6:53:bf"
 MODEL_NBR_UUID = "FFE1"
 
-async def main(address):
-    async with BleakClient(address) as client:
-        model_number = await client.read_gatt_char(MODEL_NBR_UUID)
-        print("Model Number: {0}".format("".join(map(chr, model_number))))
-        print("Available Services: {0}".format(str(client.services.get_service("FFE0"))))
-        #print("Now pairing...")
-        #await client.pair()
-        print("Starting notifs...")
-        await client.start_notify(MODEL_NBR_UUID, callback)
-        print("Recieving frames...")
-        await check_frames()
-        print("Disconnecting...")
-        #await client.unpair()
+class ConnectState(Enum):
+    NO_CONNECT = 0
+    CONNECT_INPROG = 1
+    CLIENT_CONNECTED = 2
+    DISCONNECT_INPROG = 3
+    EXIT = 4
 
-frames_recieved = 0
-async def check_frames():
-    global frames_recieved
-    while frames_recieved < 5:
-        pass
-    return
+client : BleakClient = None
+state : ConnectState = ConnectState.NO_CONNECT
 
+async def main(addr):
+    global state, client
+    print("INITIALIZING!")
+    while True:
+        match state:
+            case ConnectState.CONNECT_INPROG:
+                print("CONNECTING!")
+                client = BleakClient(addr)
+                print("Available Services: {0}".format(str(client.services.get_service("FFE0"))))
+                await client.start_notify(MODEL_NBR_UUID, callback=callback)
+                state = ConnectState.CLIENT_CONNECTED
+                print("CONNECTED!")
+            case ConnectState.DISCONNECT_INPROG:
+                print("DISCONNECTING!")
+                await client.stop_notify(MODEL_NBR_UUID)
+                client = None
+                state = ConnectState.NO_CONNECT
+                print("DISCONNECTED!")
+            case ConnectState.EXIT:
+                print("EXITING!")
+                if client is not None:
+                    print("DISCONNECTING!")
+                    await client.stop_notify(MODEL_NBR_UUID)
+                    client = None
+                print("EXITED!")
+                break
+                
 
 def callback(sender: BleakGATTCharacteristic, data: bytearray):
-        global frames_recieved
         print(f"{sender}: {data}")
-        frames_recieved += 1
 
-
-asyncio.run(main(address))
+def start():
+    asyncio.run(main(address))
