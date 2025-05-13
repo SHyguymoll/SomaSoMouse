00b787cfc9bc5f9e3d7177f240b271e7e263bd85
SHyguymoll <shymollet@gmail.com>
2025-05-12 17:28:56 -0700

    stuffed structured data into ui.py

--- ui.py
+++ ui.py
@@ -16,6 +16,12 @@ import bleak
 
 address = "f8:2e:0c:a6:53:bf"
 
+import struct
+
+FINGER1_HEADER = bytearray(b'\xF1\xF1')
+FINGER2_HEADER = bytearray(b'\xF2\xF2')
+ROTATION_HEADER = bytearray(b'\xF3\xF3')
+EXTRA_HEADER = bytearray(b'\xF4\xF4')
 
 class ExampleApp(App):
     def __init__(self):
@@ -31,7 +37,7 @@ class ExampleApp(App):
         return self.scrollview
 
     def line(self, text, empty=False):
-        Logger.info("example:" + text)
+        #Logger.info("example:" + text)
         if self.label is None:
             return
         text += "\n"
@@ -44,8 +50,21 @@ class ExampleApp(App):
         self.running = False
 
 
-    def callback(self,sender, data):
-        self.line(f"{str(data)}")
+    def callback(self, sender, data):
+        if data.startswith(FINGER1_HEADER):
+            thumb, pointer, middle, ring = struct.unpack_from("4f", data, 2)
+            self.line(f"thumb = {thumb}, pointer = {pointer}, middle = {middle}, ring = {ring}")
+        elif data.startswith(FINGER2_HEADER):
+            pinky, ax1, ay1, az1 = struct.unpack_from("4f", data, 2)
+            self.line(f"pinky = {pinky}, acceleration = ({ax1}, {ay1}, {az1})")
+        elif data.startswith(ROTATION_HEADER):
+            gx1, gy1, gz1, radX = struct.unpack_from("4f", data, 2)
+            self.line(f"angular velocity = ({gx1}, {gy1}, {gz1}), inclination x = {radX}")
+        elif data.startswith(EXTRA_HEADER):
+            radY = struct.unpack_from("f", data, 2)
+            self.line(f"inclination y = {radY}")
+        else:
+            self.line("!!!!malformed packet!!!!")
 
     async def example(self):
         self.line(f"Connected")
@@ -54,6 +73,7 @@ class ExampleApp(App):
 
 async def main(app):
     await asyncio.gather(app.async_run("asyncio"), app.example())
+    await app.client.disconnect()
 
 
 if __name__ == "__main__":
